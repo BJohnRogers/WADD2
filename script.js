2e9c4686b6c4b628c7e25c499e9d538835e14fd7
Brandon Rogers <55049486+BJohnRogers@users.noreply.github.com>
2024-03-27 17:22:30 +0000

    Fixed visibility issues with menus

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+var loggedIn;
+
 function loginMenu() {
+    var loginMenu = document.getElementById("login");
+    loginMenu.classList.toggle("show");
+}
+
+function loginMenuTwo() {
     var loginMenu = document.getElementById("login");
     var signUpMenu = document.getElementById("signUp");
     loginMenu.classList.toggle("show");
+    signUpMenu.classList.toggle("show");
 }
 
 function signUpMenu() {
     var signUpMenu = document.getElementById("signUp");
     var loginMenu = document.getElementById("login");
     signUpMenu.classList.toggle("show");
-}
-
-function loginEnter() {
-
+    loginMenu.classList.toggle("show");
 }
 
 function signUpEnter(){
 
     var getPassW = document.getElementById("passW");
     var getUserN = document.getElementById("userN");
+    var signUpMenu = document.getElementById("signUp");
+    var loginMenu = document.getElementById("login");
+    loginMenu.classList.toggle("show");
+    signUpMenu.classList.toggle("show");
     var passW;
     var userN;
    
@@ -28,6 +37,19 @@ function signUpEnter(){
         userN,
         passW
     }
+
+
+
+}
+
+function loginEnter() {
+
+    if(userN, passW = userDetails){
+        loggedIn = true;
+    }
+
+    var loginMenu = document.getElementById("login");
+    loginMenu.classList.toggle("show");
 
 }
 
